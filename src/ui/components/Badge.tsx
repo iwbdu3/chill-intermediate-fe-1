078431e9ae7ee5182bb780a4bfd684be25341f48
arrifat <arifatdhivanahara916@gmail.com>
2026-02-14 14:43:04 +0700

    changes in default badge

--- src/ui/components/Badge.tsx
+++ src/ui/components/Badge.tsx
@@ -11,7 +11,7 @@ const Badge = ({
 }: BadgeProps) => {
   // Mapping Style berdasarkan Variant
   const variants = {
-    default: "bg-transparent border-white/50",
+    default: "bg-transparent border-white/50 sm:py-2.5 py-1.5 sm:px-2.5 px-1.5",
     "new-episode": "bg-blue-800 border-none font-bold px-3 sm:px-4",
     "top-10":
       "bg-red-800 border-none font-bold rounded-tr-sm rounded-br-none rounded-tl-none rounded-bl-sm px-2 w-5 text-center sm:w-8 sm:px-3",
